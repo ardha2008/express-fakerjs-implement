5cf52606aafe33ff82368f9b45d5ff442709fe22
Ardha <ardha2008@gmail.com>
2017-12-03 18:42:53 +0700

    Add data sample table

--- routes/generator.js
+++ routes/generator.js
@@ -18,4 +18,36 @@ router.get('/sample/data', function(req, res, next) {
 	res.json(dataRandom);
 });
 
+router.get('/sample/table', function(req, res, next) {
+	var data = new Array();
+	var total = 1000;
+
+	for(var i=0;i <= total ;i++){
+		var dataRandom = {
+			name : faker.name.findName(),
+			phone : faker.phone.phoneNumber(),
+			email : faker.internet.email(),
+			finance : {
+				account : faker.finance.account(),
+				accountName : faker.finance.accountName(),
+				amount : faker.finance.amount(),
+			},
+			company : {
+				companyName : faker.company.companyName(),
+				companySuffix: faker.company.companySuffix()
+			},
+			address : {
+				street : faker.address.streetName(),
+				city : faker.address.city(),
+				state : faker.address.state(),
+				country : faker.address.country(),
+			}
+		}
+
+		data[i] = dataRandom;
+	}
+
+	res.json(data);
+});
+
 module.exports = router;
